95fecde4536a09c9fc8dc6de18192b45407ec43f
wenguang pan <wenguangpan@wenguangdeMacBook-Pro.local>
2017-05-25 18:26:00 +0800

    print重定向到文件

--- fork-dup-exec-daemon/print.c
+++ fork-dup-exec-daemon/print.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <time.h>
+#include <unistd.h>
 
 int main()
 {
-	int i = 0;
-	//for (i = 0; i < 10; ++i)
-	while(++i)
+	int fd = open("print.txt", O_CREAT | O_RDWR | O_TRUNC);
+	if (fd == -1)
 	{
-		printf("%d\n", i);
+		printf("! open failed.\n");
+		return -1;
+	}
+	if (dup2(fd, STDOUT_FILENO) == -1)
+	{
+		printf("! dup2 failed.\n");
+		return -1;
+	}
+
+	int i = 0;
+	printf("\n-----------  %s  -----------\n\n", asctime(NULL));
+	for (i = 0; i < 3; ++i)
+	{
+		printf("- daemon print message line %d\n", i);
 	}
 	return 0;
 }
