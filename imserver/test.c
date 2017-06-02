b0da3d37afa6edcc9a8a13825e36cd10c2235e57
wenguang pan <wenguangpan@wenguangdeMacBook-Pro.local>
2017-06-02 18:38:59 +0800

    yy

--- imserver/test.c
+++ imserver/test.c
@@ -6,62 +6,10 @@
 #include <ifaddrs.h>
 #include <string.h>
 #include <netinet/in.h>
-
-int getaddr(struct sockaddr *sa)
-{
-    struct ifaddrs *addrs;
-    const struct ifaddrs *cursor;
-    
-    if (getifaddrs(&addrs) < 0)
-    {
-        printf("! getifaddrs() failed.\n");
-        exit(-1);
-    }
-
-    cursor = addrs;
-    while(cursor)
-    {
-        char* enp0s3 = "enp0s3";
-        if (cursor->ifa_addr->sa_family == AF_INET && strcmp(cursor->ifa_name, enp0s3) == 0)
-        {
-            memcpy(sa, cursor->ifa_addr, (size_t)sizeof(sa));
-            printf("- %s\n", cursor->ifa_name);
-	    break;
-        }
-        cursor = cursor->ifa_next;
-    }
-    
-    free(addrs);
-
-}
+#include <map>
 
 int main(int argc, char* argv[])
 {
-
-    struct sockaddr sa;
-    getaddr(&sa);
-
-	// struct ifaddrs *addrs;
-	// const struct ifaddrs *cursor;
-	// struct sockaddr sockaddr;
-	
-	// if (getifaddrs(&addrs) < 0)
-	// {
-	// 	printf("! getifaddrs() failed.\n");
-	// 	exit(-1);
-	// }
-
-	// cursor = addrs;
- //        while(cursor)
- //        {
-	// 	//memcpy(&sockaddr, cursor->ifa_addr, (size_t)sizeof(sockaddr));
- //        	//printf("- %s\n", cursor->ifa_name);
-	// 	if (cursor->ifa_addr->sa_family == AF_INET)
-	// 		printf("- %s\n", cursor->ifa_name);
- //      		cursor = cursor->ifa_next;
-	// }
-	
-	// free(addrs);
-
+    Map<int, string> mapSt;
 	return 0;
 }
